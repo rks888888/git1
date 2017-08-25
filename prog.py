40691861bfbf99df1e367f25009a3aca2b98e545
rks888888 <robert1sturgill@gmail.com>
2017-08-25 11:08:36 -0400

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,23 @@
+#program 1
+print ("Hello World!")
+
+#program 2
+greeting = "Hello, this is my use of variables and strings!"
+print (greeting)
+
+#program 3
+firstName = input("What is your First Name?")
+lastName = input("What is your last name?")
+greet_user = "Hello "
+
+print (greet_user + firstName.title() + " " + lastName.title() + "!")
+
+#program 4
+
+print ('This is how we present quotes, "I can not tell a lie. - George Washington"')
+
+#program 5
+Famous_Quote = 'George Washington once said, "I can not tell a lie."'
+
+print(Famous_Quote)
+
