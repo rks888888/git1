1425fb98ed741175ecabc996f77df272ab56fc74
rks888888 <robert1sturgill@gmail.com>
2018-02-13 14:24:51 -0500

    updates
    updated program

--- engineprog.py
+++ engineprog.py
@@ -5,7 +5,7 @@ print ('B - Carb Calculation i.e. the best carb for the application')
 
 ans= input('A or B')
 
-if ans is 'A':
+if ans == 'A' or ans == 'a':
 
   print ('example bore x bore x stroke x number of cylinders x .7854 = CID')
 
@@ -18,9 +18,28 @@ if ans is 'A':
   bor = input("What bore do you have:")
   bore = float(bor)
   engine = (bore * bore * stroke * cylinders * .7854)
-  eng = str(engine)
-  print ('The cubic inces of your block will be, ' + eng + ' thank you for your time.')
+  engine2 = round(engine)
+  eng = str(engine2)
+  print ('The cubic inches of your block will be, ' + eng + ' CI thank you for your time.')
+  print ('would you like to run through the other selection at this time?')
+  other=input('Yes or No')
+  if other == 'yes' or other == "Yes" or other == "YES":
+    print ('Stock Engines Volumetric Efficency or VE = 0.85')
+    print ('Mild Built Volumetric Efficency or VE = 0.90')
+    print ('Racing Engines Volumetric Efficency or VE 1.0')
 
+    EngCID = input("What CID is your engine:")
+    EngineCID = int(EngCID)
+    RPM = input ("What is the max RPM of your engine:")
+    MXRPM = int(RPM)
+    VolEff = input('What is your VE:')
+    VE = float(VolEff)
+    CarbCFM = ((EngineCID * MXRPM * VE) / 3456)
+    carb = str(CarbCFM)
+    print('The correct carburetor you will need ' + carb + ' cfm to function correctly')
+  else:
+    print('Thank you for using the calculator!')
+    
 else:
 
   print ('Stock Engines Volumetric Efficency or VE = 0.85')
@@ -34,5 +53,7 @@ else:
   VolEff = input('What is your VE:')
   VE = float(VolEff)
   CarbCFM = ((EngineCID * MXRPM * VE) / 3456)
-  carb = str(CarbCFM)
+  CarbCFM2 = round(CarbCFM)
+  carb = str(CarbCFM2)
   print('The correct carburetor you will need ' + carb + ' cfm to function correctly')
+  print('Thank you for using the calculator!')
